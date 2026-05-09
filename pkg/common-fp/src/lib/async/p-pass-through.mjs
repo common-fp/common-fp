f8e5f2ea91631da465e6ff2c888e3a8e76a64b9c
Phil Olson <olsonpm@users.noreply.github.com>
2026-05-09 18:47:09 -0500

    Revert "await promise value for pPassThrough"
    This reverts commit 1d0047ea64ca626782f4f80639186732e1b24084
    
    this will go in the next minor version, so reverting for now.

--- pkg/common-fp/src/lib/async/p-pass-through.mjs
+++ pkg/common-fp/src/lib/async/p-pass-through.mjs
@@ -3,7 +3,7 @@ import { assertArgIsArrayOfType } from '@common-fp/shared-internals'
 const pPassThrough = async (value, fnArray) => {
   assertArgIsArrayOfType(fnArray, 'fnArray', 'function', 'pPassThrough')
 
-  let result = await value
+  let result = value
   for (const fn of fnArray) result = await fn(result)
 
   return result
